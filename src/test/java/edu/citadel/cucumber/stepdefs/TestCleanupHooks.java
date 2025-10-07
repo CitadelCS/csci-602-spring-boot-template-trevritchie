48ae08d96aa929d2fd14ce70ac0f6f94da0e0373
Trevor Ritchie <trevritchie@gmail.com>
2025-10-07 16:36:15 -0400

    Refactor TestCleanupHooks to remove unused AccountRepository and redundant afterScenario cleanup. Streamlined test context management by retaining unique usernames for tests.

--- src/test/java/edu/citadel/cucumber/stepdefs/TestCleanupHooks.java
+++ src/test/java/edu/citadel/cucumber/stepdefs/TestCleanupHooks.java
@@ -1,10 +1,8 @@
 package edu.citadel.cucumber.stepdefs;
 
-import edu.citadel.dal.AccountRepository;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Cucumber hooks for test cleanup and setup.
@@ -13,23 +11,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class TestCleanupHooks {
 
     @Autowired
-    private AccountRepository accountRepository;
-
-    @Autowired
     private TestContext testContext;
 
     @Before
     public void beforeScenario() {
         // Clear the test context before each scenario
+        // Tests use unique usernames to avoid conflicts with existing data
         testContext.clear();
-    }
-
-    @After
-    public void afterScenario() {
-        // Clear the test context after each scenario
-        testContext.clear();
-
-        // Note: We don't clean up the database to avoid affecting production data
-        // Tests use unique usernames to prevent conflicts with existing data
     }
 }
