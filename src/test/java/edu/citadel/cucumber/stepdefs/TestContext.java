31bbd2e0d59e6a962ac65640639a18b17cab181f
Trevor Ritchie <trevritchie@gmail.com>
2025-10-07 18:31:02 -0400

    Use lombox getters and setters for TestContext

--- src/test/java/edu/citadel/cucumber/stepdefs/TestContext.java
+++ src/test/java/edu/citadel/cucumber/stepdefs/TestContext.java
@@ -2,6 +2,8 @@ package edu.citadel.cucumber.stepdefs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.citadel.dal.model.Account;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
@@ -14,68 +16,30 @@ import java.util.Map;
  * This class holds response objects and test data that need to be shared
  * between different step definition classes.
  */
+@Getter
 @Component
 public class TestContext {
-    
-    // Response objects
+
+    // Getters and setters for response objects
+    @Setter
     private ResponseEntity<String> stringResponse;
+    @Setter
     private ResponseEntity<Account> accountResponse;
-    
-    // Test data
+
+    // Getters and setters for test data
+    @Setter
     private Account createdAccount;
     private final Map<String, Account> createdAccounts = new HashMap<>();
-    
-    // Utilities
+
+    // Utility getters
     private final ObjectMapper objectMapper = new ObjectMapper();
+    @Setter
     private StopWatch stopWatch;
-    
-    // Getters and setters for response objects
-    public ResponseEntity<String> getStringResponse() {
-        return stringResponse;
-    }
-    
-    public void setStringResponse(ResponseEntity<String> stringResponse) {
-        this.stringResponse = stringResponse;
-    }
-    
-    public ResponseEntity<Account> getAccountResponse() {
-        return accountResponse;
-    }
-    
-    public void setAccountResponse(ResponseEntity<Account> accountResponse) {
-        this.accountResponse = accountResponse;
-    }
-    
-    // Getters and setters for test data
-    public Account getCreatedAccount() {
-        return createdAccount;
-    }
-    
-    public void setCreatedAccount(Account createdAccount) {
-        this.createdAccount = createdAccount;
-    }
-    
-    public Map<String, Account> getCreatedAccounts() {
-        return createdAccounts;
-    }
-    
+
     public void addCreatedAccount(String username, Account account) {
         createdAccounts.put(username, account);
     }
-    
-    // Utility getters
-    public ObjectMapper getObjectMapper() {
-        return objectMapper;
-    }
-    
-    public StopWatch getStopWatch() {
-        return stopWatch;
-    }
-    
-    public void setStopWatch(StopWatch stopWatch) {
-        this.stopWatch = stopWatch;
-    }
-    
+
     // Helper method to get the current response (either string or account)
     public ResponseEntity<?> getCurrentResponse() {
         if (accountResponse != null) {
